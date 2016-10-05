fd343af0332ca74d208a19d8011b81f2e3535913
Ellen Marie Dash <me@duckie.co>
2016-10-05 03:09:49 -0400

    MASSIVE PROGRESS.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{self, Write};
+
 fn parse_line(string: String) -> Vec<String> {
     let mut tokens = Vec::new();
 
@@ -12,16 +14,22 @@ fn execute_line(parts: Vec<String>) {
     let ref command = parts[0];
     let ref args    = &parts[1..];
 
-    print!("{} {:?}", command, args);
+    println!("{} {:?}", command, args);
 }
 
 fn main() {
     loop {
         let mut input = String::new();
 
-        match std::io::stdin().read_line(&mut input) {
+        print!("$ ");
+
+        io::stdout().flush().unwrap();
+
+        match io::stdin().read_line(&mut input) {
             Ok(n) => execute_line(parse_line(input)),
             Err(error) => println!("error: {}", error),
         }
+
+        io::stdout().flush().unwrap();
     }
 }
